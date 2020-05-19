1c573077f842f47b81994964de0c38c7e7678e34
vweltje <vincentweltje@gmail.com>
2020-05-19 13:43:30 +0200

    Cursort pointer on buttons

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -5,14 +5,12 @@ import ProjectSubmit from "./ProjectSubmit/ProjectSubmit"
 
 const ProjectForm = () => {
   return (
-    <>
+    <form autoComplete="off">
       <ProjectInfo />
       <ProjectDetails />
       <ProjectSubmit />
-    </>
+    </form>
   )
 }
 
 export default ProjectForm
-
-// <ProjectComment />
